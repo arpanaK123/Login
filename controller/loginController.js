1e6e2c7d5a47609046d6c38ad9c571880bd4847b
arpana kumari <arpanabitp1317@gmail.com>
2018-05-29 18:10:30 +0530

    program

--- controller/loginController.js
+++ controller/loginController.js
@@ -1,8 +1,24 @@
 app.controller('loginCtrl',function($scope,$state){
+  $scope.isMatchpasswordwithemail=false;
 $scope.submit = function(){
+
   $state.go('home');
   console.log($scope.email);
   console.log($scope.password);
 };
-
+$scope.comparePassword=function(password,userEmail){
+  if(password && email)
+  {
+    const emailString=userEmail.subString(0,3).trim().toLowerCase();
+    const passwordString=password.subString(0,3).trim().toLowerCase();
+    if(emailString!==passwordString)
+    {  $scope.isMatchpasswordwithemail=false;
+     console.log(password not match);
+    }
+    else
+      {  $scope.isMatchpasswordwithemail=true;
+        console.log(password match);
+      }
+    }
+  };
 });
