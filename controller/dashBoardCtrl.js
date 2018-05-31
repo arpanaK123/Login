785919c2714b66c8f41fbc6ca7d101aea254ddf6
arpana kumari <arpanabitp1317@gmail.com>
2018-05-31 17:22:50 +0530

    pending

--- controller/dashBoardCtrl.js
+++ controller/dashBoardCtrl.js
@@ -1,22 +1,18 @@
-app.controller('dashBoardCtrl',function($scope,$mdDialog){
-  $scope.showMore = function(event,info)
-  {
+app.controller('dashboardController', function ($scope, $mdDialog) {
+  $scope.showMore = function(event,info) {
     $mdDialog.show({
-    locals:(data:info),
-    controller:dialogController,
-    templateUrl: 'template/dialogBox.html',
-    parent: angular.element(document.body),
-    targetEvent: event,
-     clickOutsideToClose:true;
-  });
-
+      locals:{data : info},
+      controller: dialogController,
+      templateUrl: 'template/dialogbox.html',
+       parent: angular.element(document.body),
+      targetEvent: event,
+      clickOutsideToClose:true,
+    });
 };
-function dialogController($scope,data,$mdDialog)
-{
-  $scope.data=data;
-  $scope.cancel=function()
-  {
-    $mdDialog.cancel();
-  };
+function dialogController($scope, data, $mdDialog) {
+$scope.data = data;
+$scope.cancel = function() {
+$mdDialog.cancel();
+};
 }
 });
