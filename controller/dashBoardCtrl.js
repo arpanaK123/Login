0ed8194e9da8a5bad2cbb1a41ef0a15d206abcc9
arpana kumari <arpanabitp1317@gmail.com>
2018-05-31 16:27:56 +0530

    programs

--- controller/dashBoardCtrl.js
+++ controller/dashBoardCtrl.js
@@ -1,2 +1,21 @@
-app.controller('dashBoardCtrl',function($scope){
+app.controller('dashBoardCtrl',function($scope,$mdDialog){
+  $scope.showMore = function(event,info)
+  {$mdDialog.show({
+    locals:(data:info),
+    controller:dialogCOntroller,
+    templateUrl: 'template/dialog.html',
+    parent: angular.element(document.body),
+    targetEvent: event,
+     clickOutsideToClose:true;
+  });
+
+};
+function dialogCOntroller($scope,data,$mdDialog)
+{
+  $scope.data=data;
+  $scope.cancel=function()
+  {
+    $mdDialog.cancel();
+  };
+}
 });
