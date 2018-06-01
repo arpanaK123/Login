d4ed74ad33ee831fbcdefb8b92827c2ed735b851
arpana kumari <arpanabitp1317@gmail.com>
2018-06-01 18:26:37 +0530

    pending

--- controller/homeCtrl.js
+++ controller/homeCtrl.js
@@ -25,44 +25,43 @@ app.controller('homeCtrl', function($scope, products, $mdSidenav) {
 });
 var specsArr = [];
 $scope.onDisplaySelectedItem = function(specsValue, allData) {
-  console.log('specsValue:'specsValue, 'allData:'allData) {
-    //finding the index of selected item
-    var index = specsArr.indexOf(specsValue)
-    if (index !== -1) {
-      //checkbox is unchecked,remove item from the specsArr
-      specsArr.splice(index, 1);
-    } else {
-      //checkbox is checked,add item in specsArr
-      specsArr.push(specsValue);
-    }
-    if (specsArr.length > 0) {
-      //if item is selected in sidebar then filter data in dashBoard
-      $scope.allMobilesInfo = allData.filter(function(obj) {
-        return specsArr.find(function(item) {
+console.log('specsValue:'
+  specsValue, 'allData:'
+  allData) {
+  //finding the index of selected item
+  var index = specsArr.indexOf(specsValue)
+  if (index !== -1) {
+    //checkbox is unchecked,remove item from the specsArr
+    specsArr.splice(index, 1);
+  } else {
+    //checkbox is checked,add item in specsArr
+    specsArr.push(specsValue);
+  }
+  if (specsArr.length > 0) {
+    //if item is selected in sidebar then filter data in dashBoard
+    $scope.allMobilesInfo = allData.filter(function(obj) {
+      return specsArr.find(function(item) {
 
 
-          if (obj.specs.manufacturer === item) {
-            return obj;
-          }
-          if(obj.specs.storage===item)
-          {
-            return obj;
-          }
-          if(obj.specs.os===item)
-          {
-            return obj;
-          }
-          if(obj.specs.camera===item)
-          {
-            return obj;
-          }})
-      });
-    }
-    else
-      {
-        $scope.allMobilesInfo=$scope.sideMenuArr;
-      }
-      console.log('filterData:',$scope.allMobilesInfo);
-      console.log('selected specific item in side menu:'specsArr);
-    };
-  }};
+        if (obj.specs.manufacturer === item) {
+          return obj;
+        }
+        if (obj.specs.storage === item) {
+          return obj;
+        }
+        if (obj.specs.os === item) {
+          return obj;
+        }
+        if (obj.specs.camera === item) {
+          return obj;
+        }
+      })
+    });
+  } else {
+    $scope.allMobilesInfo = $scope.sideMenuArr;
+  }
+  console.log('filterData:', $scope.allMobilesInfo);
+  console.log('selected specific item in side menu:'
+    specsArr);
+};
+});
