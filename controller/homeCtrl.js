428ef0611ae1539653a1d69160081cc54de912a4
arpana kumari <arpanabitp1317@gmail.com>
2018-06-04 18:22:07 +0530

    update

--- controller/homeCtrl.js
+++ controller/homeCtrl.js
@@ -1,112 +1,56 @@
-// app.controller('homeCtrl', function($scope, $state, $mdSidenav, products) {
-//   $scope.toggleLeft = buildToggler('left');
-//
-//   function buildToggler(componentId) {
-//     return function() {
-//       $mdSidenav(componentId).toggle();
-//     };
-//   }
-//
-//   $scope.onLogout = function() {
-//     $state.go('login');
-//   };
-//
-//   products.getData().then(function(response) {
-//     $scope.allMobilesInfo = response;
-//     console.log('JSON response: ', response);
-//   });
-//   $scope.manufacturerArr = [];
-//   $scope.storageArr = [];
-//   $scope.osArr = [];
-//   $scope.cameraArr = [];
-//   $scope.onDisplaySelectedItems = function(specsValue, specsType) {
-//     switch (specsType) {
-//       case 'manufacturer':
-//         onPushOrPopData(specsValue, $scope.manufacturerArr);
-//         break;
-//       case 'storage':
-//         onPushOrPopData(specsValue, $scope.storageArr);
-//         break;
-//       case 'os':
-//         onPushOrPopData(specsValue, $scope.osArr);
-//         break;
-//       case 'camera':
-//         onPushOrPopData(specsValue, $scope.cameraArr);
-//         break;
-//     }
-//
-//     console.log('manufacturerArr:', $scope.manufacturerArr);
-//     console.log('storageArr:', $scope.storageArr);
-//     console.log('osArr:', $scope.osArr);
-//     console.log('cameraArr:', $scope.cameraArr);
-//   };
-//
-//   var onPushOrPopData = function(specsValue, arr) {
-//     var index = arr.indexOf(specsValue); // finding the index of selected item.
-//     if (index !== -1) {
-//       // checkbox is unchecked, remove item from arr
-//       arr.splice(index, 1);
-//     } else {
-//       // checkbox is checked, add item in arr
-//       arr.push(specsValue);
-//     }
-//   }
-// });
-//
+app.controller('homeCtrl', function($scope, $state, $mdSidenav, products) {
+  $scope.toggleLeft = buildToggler('left');
+$scope.isSidebarOpen=false;
 
+  function buildToggler(componentId) {
+    return function() {
+      $scope.isSidebarOpen=!$scope.isSidebarOpen;
+      $mdSidenav(componentId).toggle();
+    };
+  }
 
-app.controller('homeCtrl', function ($scope,$state , $mdSidenav, products) {
-$scope.toggleLeft = buildToggler('left');
-$scope.isSidebarOpen = false;
+  $scope.onLogout = function() {
+    $state.go('login');
+  };
 
-function buildToggler(componentId) {
-return function () {
-$scope.isSidebarOpen = !$scope.isSidebarOpen;
-$mdSidenav(componentId).toggle();
-};
-}
+  products.getData().then(function(response) {
+    $scope.allMobilesInfo = response;
+    console.log('JSON response: ', response);
+  });
+  $scope.manufacturerArr = [];
+  $scope.storageArr = [];
+  $scope.osArr = [];
+  $scope.cameraArr = [];
+  $scope.onDisplaySelectedItems = function(specsValue, specsType) {
+    switch (specsType) {
+      case 'manufacturer':
+        onPushOrPopData(specsValue, $scope.manufacturerArr);
+        break;
+      case 'storage':
+        onPushOrPopData(specsValue, $scope.storageArr);
+        break;
+      case 'os':
+        onPushOrPopData(specsValue, $scope.osArr);
+        break;
+      case 'camera':
+        onPushOrPopData(specsValue, $scope.cameraArr);
+        break;
+    }
 
-$scope.onLogOut = function () {
-$state.go('login');
-};
-products.getData().then(function (response) {
-$scope.allMobilesInfo = response;
-console.log('JSON response: ', response);
-});
-$scope.manufacturerArr = [];
-$scope.storageArr = [];
-$scope.osArr = [];
-$scope.cameraArr = [];
-$scope.onDisplaySelectedItems = function (specsValue, specsType) {
-switch (specsType) {
-case 'manufacturer':
-onPushOrPopData(specsValue, $scope.manufacturerArr);
-break;
-case 'storage':
-onPushOrPopData(specsValue, $scope.storageArr);
-break;
-case 'os':
-onPushOrPopData(specsValue, $scope.osArr);
-break;
-case 'camera':
-onPushOrPopData(specsValue, $scope.cameraArr);
-break;
-}
+    console.log('manufacturerArr:', $scope.manufacturerArr);
+    console.log('storageArr:', $scope.storageArr);
+    console.log('osArr:', $scope.osArr);
+    console.log('cameraArr:', $scope.cameraArr);
+  };
 
-console.log('manufacturerArr:', $scope.manufacturerArr);
-console.log('storageArr:', $scope.storageArr);
-console.log('osArr:', $scope.osArr);
-console.log('cameraArr:', $scope.cameraArr);
-};
-
-var onPushOrPopData = function (specsValue, arr) {
-var index = arr.indexOf(specsValue); // finding the index of selected item.
-if (index !== -1) {
-// checkbox is unchecked, remove item from arr
-arr.splice(index, 1);
-} else {
-// checkbox is checked, add item in arr
-arr.push(specsValue);
-}
-}
+  var onPushOrPopData = function(specsValue, arr) {
+    var index = arr.indexOf(specsValue); // finding the index of selected item.
+    if (index !== -1) {
+      // checkbox is unchecked, remove item from arr
+      arr.splice(index, 1);
+    } else {
+      // checkbox is checked, add item in arr
+      arr.push(specsValue);
+    }
+  }
 });
