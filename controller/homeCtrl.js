e0f4a3888c8f9be4938cbad36a512dd885ee7cff
arpana kumari <arpanabitp1317@gmail.com>
2018-06-04 11:17:11 +0530

    update

--- controller/homeCtrl.js
+++ controller/homeCtrl.js
@@ -1,31 +1,3 @@
-//app.controller('homeCtrl', function($scope, products, $mdSidenav) {
-//   $scope.toggle = buildToggle('left');
-//
-//   function buildToggle(componentId) {
-//     return function() {
-//       $mdSidenav(componentId)
-//         .toggle();
-//     };
-//   }
-//   products.getData()
-//     .then(function(response) {
-//       $scope.allMobilesInfo = response;
-//       $scope.sideMenuArr = response;
-//       console.log('$scope.jsonData', $scope.allMobilesInfo);
-//     })
-//
-//   products.getData();
-//   $scope.toggleLeft = function() {
-//     $mdSidenav('left')
-//       .toggle();
-//
-//
-//   }
-//   console.log("hii !");
-// // }
-//
-// });
-
 app.controller('homeCtrl', function($scope, $mdSidenav, products) {
   $scope.toggleLeft = buildToggler('left');
 
