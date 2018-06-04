e4629d112f1006322291e04054f36b53fbe069ba
arpana kumari <arpanabitp1317@gmail.com>
2018-06-04 10:01:38 +0530

    pending

--- controller/homeCtrl.js
+++ controller/homeCtrl.js
@@ -1,64 +1,80 @@
-app.controller('homeCtrl', function($scope, products, $mdSidenav) {
-  $scope.toggle = buildToggle('left');
+//app.controller('homeCtrl', function($scope, products, $mdSidenav) {
+//   $scope.toggle = buildToggle('left');
+//
+//   function buildToggle(componentId) {
+//     return function() {
+//       $mdSidenav(componentId)
+//         .toggle();
+//     };
+//   }
+//   products.getData()
+//     .then(function(response) {
+//       $scope.allMobilesInfo = response;
+//       $scope.sideMenuArr = response;
+//       console.log('$scope.jsonData', $scope.allMobilesInfo);
+//     })
+//
+//   products.getData();
+//   $scope.toggleLeft = function() {
+//     $mdSidenav('left')
+//       .toggle();
+//
+//
+//   }
+//   console.log("hii !");
+// // }
+//
+// });
 
-  function buildToggle(componentId) {
+app.controller('homeCtrl', function($scope, $mdSidenav, products) {
+  $scope.toggleLeft = buildToggler('left');
+
+  function buildToggler(componentId) {
     return function() {
-      $mdSidenav(componentId)
-        .toggle();
+      $mdSidenav(componentId).toggle();
     };
   }
-  products.getData()
-    .then(function(response) {
-      $scope.allMobilesInfo = response;
-      $scope.sideMenuArr = response;
-      console.log('$scope.jsonData', $scope.allMobilesInfo);
-    })
 
-  products.getData();
-  $scope.toggleLeft = function() {
-    $mdSidenav('left')
-      .toggle();
+  products.getData().then(function(response) {
+    $scope.allMobilesInfo = response;
+    $scope.sideMenuArr = response;
+    console.log('JSON response: ', response);
+  });
 
-
-  }
-  console.log("hii !");
-// });
-var specsArr = [];
-$scope.onDisplaySelectedItem = function(specsValue, allData) {
-
-  //finding the index of selected item
-  var index = specsArr.indexOf(specsValue)
-  if (index !== -1) {
-    //checkbox is unchecked,remove item from the specsArr
-    specsArr.splice(index, 1);
-  } else {
-    //checkbox is checked,add item in specsArr
-    specsArr.push(specsValue);
-  }
-  if (specsArr.length > 0) {
-    //if item is selected in sidebar then filter data in dashBoard
-    $scope.allMobilesInfo = allData.filter(function(obj) {
-      return specsArr.find(function(item) {
-
-
-        if (obj.specs.manufacturer === item) {
-          return obj;
-        }
-        if (obj.specs.storage === item) {
-          return obj;
-        }
-        if (obj.specs.os === item) {
-          return obj;
-        }
-        if (obj.specs.camera === item) {
-          return obj;
-        }
-      })
-    });
-  } else {
-    $scope.allMobilesInfo = $scope.sideMenuArr;
-  }
-  console.log('filterData:', $scope.allMobilesInfo);
-  console.log('selected specific item in side menu:',specsArr);
-}
+  $scope.onDisplaySelectedItems = function(specsValue, allData) {
+    console.log('specsValue:', specsValue, ', allData:', allData);
+    var index = specsArr.indexOf(specsValue); // finding the index of selected item.
+    if (index !== -1) {
+      // checkbox is unchecked, remove item from specsArr
+      specsArr.splice(index, 1);
+    } else {
+      // checkbox is checked, add item in specsArr
+      specsArr.push(specsValue);
+    }
+    if (specsArr.length > 0) { // if item is selected in sidebar, then filter data in dashboard.
+      // The filter() method creates a new array with all matched elements.
+      $scope.allMobilesInfo = allData.filter(function(obj) {
+        //The find() method returns the value of the first match element in the array.
+        return specsArr.find(function(item) {
+          if (obj.specs.manufacturer === item) {
+            return obj;
+          }
+          if (obj.specs.storage === item) {
+            return obj;
+          }
+          if (obj.specs.os === item) {
+            return obj;
+          }
+          if (obj.specs.camera === item) {
+            return obj;
+          }
+        })
+      });
+    } else {
+      // nothing is selected in sidebar, display all data.
+      $scope.allMobilesInfo = $scope.sideMenuArr;
+    }
+    console.log('Filtered Data:', $scope.allMobilesInfo);
+    console.log('Selected Specific item in side menu:', specsArr);
+  };
 });
