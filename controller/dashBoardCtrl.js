457859ba8b72cbdc88eba47f645f6e98e5fb9916
arpana kumari <arpanabitp1317@gmail.com>
2018-06-05 19:05:41 +0530

    pending

--- controller/dashBoardCtrl.js
+++ controller/dashBoardCtrl.js
@@ -11,11 +11,11 @@ app.controller('dashboardController', function($scope, $mdDialog) {
       clickOutsideToClose: true,
     });
   };
-  $scope.star=[{url: 'bower_components/material-design-icons/toggle/svg/design/star.svg'},
-  {url: 'bower_components/material-design-icons/toggle/svg/design/star.svg'},
-  {url: 'bower_components/material-design-icons/toggle/svg/design/star.svg'},
-  {url: 'bower_components/material-design-icons/toggle/svg/design/star.svg'},
-  {url: 'bower_components/material-design-icons/toggle/svg/design/star.svg'}];
+  $scope.stars=[{url: 'bower_components/material-design-icons/toggle/svg/design/a.svg'},
+  {url: 'bower_components/material-design-icons/toggle/svg/design/a.svg'},
+  {url: 'bower_components/material-design-icons/toggle/svg/design/a.svg'},
+  {url: 'bower_components/material-design-icons/toggle/svg/design/a.svg'},
+  {url: 'bower_components/material-design-icons/toggle/svg/design/a.svg'}];
 
   function dialogController($scope, data, $mdDialog) {
     $scope.data = data;
