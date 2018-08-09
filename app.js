3e62d7bb157a4b304990d772f5eda56689439188
arpana kumari <arpanabitp1317@gmail.com>
2018-08-09 19:18:23 +0530

    programs

--- app.js
+++ app.js
@@ -7,11 +7,23 @@ app.config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $ur
       templateUrl: 'template/login.html',
       controller: 'loginCtrl'
     })
+    .state('register', {
+      url: '/register',
+      templateUrl: 'template/register.html',
+      controller: 'registerCtrl'
+    })
+
+    .state('userverify', {
+      url: '/userverify',
+      templateUrl: 'template/userverify.html',
+      controller: 'userverifyCtrl'
+    })
     .state('home', {
       url: '/home',
       templateUrl: 'template/home.html',
       controller: 'homeCtrl'
     })
+
     .state('home.dashBoard', {
       url: '/dashBoard',
       templateUrl: 'template/dashBoard.html',
